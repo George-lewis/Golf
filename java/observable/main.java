3ad38de5dc4facae2a4c3a03d1a13499dd7a6bac
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-30 19:26:32 -0500

    Add explanation

--- java/observable/main.java
+++ java/observable/main.java
@@ -1,3 +1,18 @@
+//
+// The goal is to reimplement Observer and Observable myself
+// Then demonstrate its functionality
+//
+// The classes Observer and Observable have been implemented by me
+// Switch implements Observable and Light implements Observer
+// Ergo lights observe switches
+//
+// When a switch is flipped on it will notify all of its connected
+// Lights and so they will light up as well
+//
+// In theory you could have one light connected to multiple switches
+// For a more complicated system
+//
+
 public class main {
 
     public static void main(String[] args) {
