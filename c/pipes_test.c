64513bf45ad9eb59a9105974469de84ffbe4d82f
George-Lewis <georgejeffreylewis@gmail.com>
2020-02-22 21:10:25 -0500

    Fix pipes

--- c/pipes_test.c
+++ c/pipes_test.c
@@ -3,10 +3,10 @@
 //////////////////
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
 
@@ -14,22 +14,31 @@ int main() {
 	pipe(fd);
 
 	if (!fork()) {
-  		char *msg = "Hello, world!";
+	
+		close(fd[0]); // We're not reading in the child
+		
+		char *msg = "Hello, world!";
   		write(fd[1], msg, strlen(msg));
-  		close(fd[1]);
-  		exit(EXIT_SUCCESS);
+  		close(fd[1]); // Done writing
+  	
+		exit(EXIT_SUCCESS);
+
+	} else {
+	
+		close(fd[1]); // Not writing in the parent
+
+		wait(NULL); // Not strictly necessary but will guarantee we're good
+	
+		char buf[30] = { 0 };
+	
+		read(fd[0], buf, 30);
+	
+		close(fd[0]);
+	
+		printf("%s\n", buf);
+	
+		exit(EXIT_SUCCESS);
+
 	}
-	
-	wait(NULL);
-	
-	char buf[30] = { 0 };
-	
-	read(fd[0], buf, 30);
-	
-	close(fd[0]);
-	
-	printf("%s\n", buf);
-	
-	exit(EXIT_SUCCESS);
 
 }
